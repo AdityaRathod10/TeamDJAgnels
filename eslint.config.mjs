939086ea4ed0ae81edbe7ea8a60f2f5b05f94017
AdityaRathod10 <adityarathod2704@gmail.com>
2025-01-30 10:57:08 +0530

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',       // Disable unused-vars error
+      '@typescript-eslint/no-explicit-any': 'off',     // Disable any type warning
+    },
+  },
 ];
 
 export default eslintConfig;
